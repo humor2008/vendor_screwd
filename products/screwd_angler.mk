2d8f8c43a54075a546dab75b98eae4849534ba9e
f100cleveland <dustin.rinne@gmail.com>
2016-04-18 08:21:59 -0500

    Setup for sabermod tc

--- products/screwd_angler.mk
+++ products/screwd_angler.mk
@@ -39,19 +39,58 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 
 # Inline kernel building
-TARGET_GCC_VERSION_ARM64 := 6.0
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := saber_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 #Optimize-it!!
-export STRICT_ALIASING := true
-export ENABLE_GCCONLY := true
-export GRAPHITE_OPTS := true
-export CLANG_O3 := true
+# System
+export USE_CCACHE := 1
+export USE_SYSTEM_CCACHE := 1
+export BLOCK_BUILD := true
+
+# Build
+export ANDROID_COMPILE_WITH_JACK := false
+export DISABLE_OPTIMIZATIONS := false
+ifneq ($(DISABLE_OPTIMIZATIONS),true)
+export DONT_ERROROUT := false
+export USE_O3_OPTIMIZATIONS := false
+export FORCE_DISABLE_DEBUGGING := true
+export ENABLE_IPA_ANALYSER := true
+export TARGET_USE_PIPE := true
 export CORTEX_TUNINGS := true
+export ENABLE_PTHREAD := true
+export ENABLE_GOMP := true
+export GRAPHITE_OPTS := true
+export ENABLE_EXTRAGCC := true
+export STRICT_ALIASING := true
 export ENABLE_SANITIZE := true
-export USE_PIPE := true
+endif
+
+# Toolchain and other
+export TARGET_NDK_GCC_VERSION := 4.9
+export TARGET_GCC_VERSION_EXP := 4.9
+export TARGET_GCC_VERSION := 4.9
+export TARGET_GCC_VERSION_KERNEL := 7.0
+
+# Prop Optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.performance.tuning=1 \
+    keyguard.no_require_sim=true \
+    persist.service.lgospd.enable=0 \
+    persist.service.pcsync.enable=0 \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false \
+    ro.ril.power_collapse=1 \
+    ro.sys.fw.bg_apps_limit=20 \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    wifi.supplicant_scan_interval=180 \
+    windowsmgr.max_events_per_sec=150
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	DEVICE_MAINTAINERS="David Smit (dsmitty166) & Dustin Rinne (f100cleveland)"
