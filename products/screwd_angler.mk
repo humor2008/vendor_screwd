2baa5e7fb8db96855a0d5ca193125eda1cd430f8
f100cleveland <dustin.rinne@gmail.com>
2016-04-18 11:32:21 -0500

    Removed ccache

--- products/screwd_angler.mk
+++ products/screwd_angler.mk
@@ -44,11 +44,6 @@ TARGET_KERNEL_CONFIG := saber_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 #Optimize-it!!
-# System
-export USE_CCACHE := 1
-export USE_SYSTEM_CCACHE := 1
-export BLOCK_BUILD := true
-
 # Build
 export ANDROID_COMPILE_WITH_JACK := false
 export DISABLE_OPTIMIZATIONS := false
