ab37b5a6e029c225328f2bcc6988f2d0fd2e28ee
mydongistiny <jasonedson@gmail.com>
2016-04-18 08:01:52 -0500

    screwd.mk: Change around flags and add ThinLTO

--- config/optimizations/screwd.mk
+++ config/optimizations/screwd.mk
@@ -28,45 +28,34 @@
 
  HOST_OS := linux
 
- TARGET_ARCH_LIB_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_SM_AND)/lib
- export TARGET_ARCH_LIB_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-$(TARGET_SM_AND)/lib
+  TARGET_ARCH_LIB_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_SM_AND)/lib
+  export TARGET_ARCH_LIB_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_SM_AND)/lib
 
- # Path to ROM toolchain
- SM_AND_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-$(TARGET_SM_AND)
- SM_AND := $(shell $(SM_AND_PATH)/bin/arm-linux-androideabi-gcc --version)
+  # Path to toolchain
+  SM_AND_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-$(TARGET_SM_AND)
+  SM_AND := $(shell cat $(SM_AND_PATH)/VERSION)
 
- # Find strings in version info
- SM_AND_NAME := $(filter %sabermod,$(SM_AND))
- SM_AND_DATE := $(filter 20140% 20141% 20150% 20151%,$(SM_AND))
- SM_AND_STATUS := $(filter (release) (prerelease) (experimental),$(SM_AND))
- SM_AND_VERSION := $(SM_AND_NAME)-$(SM_AND_DATE)-$(SM_AND_STATUS)
+  # Find strings in version info
+  SM_AND_NAME := $(filter %sabermod,$(SM_AND))
+  SM_AND_DATE := $(filter 20140% 20141% 20150% 20151% 20160% 20161%,$(SM_AND))
+  SM_AND_STATUS := $(filter (release) (prerelease) (experimental),$(SM_AND))
+  SM_AND_VERSION := $(SM_AND_NAME)-$(SM_AND_DATE)-$(SM_AND_STATUS)
 
- # Write version info to build.prop
- ifeq (5.1,$(TARGET_GCC_VERSION))
-   PRODUCT_PROPERTY_OVERRIDES += \
-     ro.sm.android=$(SM_AND_NAME)-$(SM_AND_DATE)-(experimental)
- else
+   # Write version info to build.prop
    PRODUCT_PROPERTY_OVERRIDES += \
      ro.sm.android=$(SM_AND_VERSION)
- endif
 
- # Path to kernel toolchain
- SM_KERNEL_PATH := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-$(TARGET_SM_KERNEL)
- SM_KERNEL := $(shell $(SM_KERNEL_PATH)/bin/arm-eabi-gcc --version)
+  # Path to kernel toolchain
+  SM_KERNEL_PATH := prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-$(TARGET_SM_KERNEL)
+  SM_KERNEL := $(shell $(SM_KERNEL_PATH)/bin/aarch64-gcc --version)
 
- SM_KERNEL_NAME := $(filter %sabermod,$(SM_KERNEL))
- SM_KERNEL_DATE := $(filter 20140% 20141% 20150% 20151%,$(SM_KERNEL))
- SM_KERNEL_STATUS := $(filter (release) (prerelease) (experimental),$(SM_KERNEL))
- SM_KERNEL_VERSION := $(SM_KERNEL_NAME)-$(SM_KERNEL_DATE)-$(SM_KERNEL_STATUS)
+  SM_KERNEL_NAME := $(filter %sabermod,$(SM_KERNEL))
+  SM_KERNEL_DATE := $(filter 20140% 20141% 20150% 20151% 20160% 20161%,$(SM_KERNEL))
+  SM_KERNEL_STATUS := $(filter (release) (prerelease) (experimental),$(SM_KERNEL))
+  SM_KERNEL_VERSION := $(SM_KERNEL_NAME)-$(SM_KERNEL_DATE)-$(SM_KERNEL_STATUS)
 
- # Write version info to build.prop
- ifeq (5.1,$(TARGET_GCC_VERSION_KERNEL))
-   PRODUCT_PROPERTY_OVERRIDES += \
-     ro.sm.kernel=$(SM_KERNEL_NAME)-$(SM_KERNEL_DATE)-(experimental)
- else
    PRODUCT_PROPERTY_OVERRIDES += \
      ro.sm.kernel=$(SM_KERNEL_VERSION)
- endif
 
  # Add extra libs for the compilers to use
  export LD_LIBRARY_PATH := $(TARGET_ARCH_LIB_PATH):$(LD_LIBRARY_PATH)
@@ -79,28 +68,24 @@ ifeq ($(GRAPHITE_OPTS),true)
    OPT2 := (graphite)
 endif
 
-ifeq ($(KRAIT_TUNINGS),true)
-   OPT3 := (krait)
+ifeq (true,$(STRICT_ALIASING))
+   OPT3 := (strict)
 endif
 
-ifeq ($(FORCE_DISABLE_DEBUGGING),true)
-   OPT4 := (no-debug)
+ifeq ($(CORTEX_TUNINGS),true)
+   OPT4 := (cortex-a57)
 endif
+
+# Used by DTC
+   OPT5 := (ThinLTO)
+   OPT6 := (polly)
 
 ifeq ($(ENABLE_PTHREAD),true)
-   OPT5 := (pthread)
+   OPT7 := (pthread)
 endif
 
 ifeq ($(ENABLE_GOMP),true)
-   OPT6 := (openmp)
-endif
-
-ifeq (true,$(ENABLE_GOLD_LINKER))
-   OPT7 := (gold-linker)
-endif
-
-ifeq (true,$(FLOOP_NEST_OPTIMIZE))
-   OPT8 := (lno)
+   OPT8 := (OpenMP)
 endif
 
 ifeq (true,$(ENABLE_EXTRAGCC))
